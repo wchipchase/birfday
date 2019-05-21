992d4a5cc67a834b88968fdfc903c8aa5fb23517
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-05-20 20:51:20 -0600

    login and register

--- src/javascripts/components/auth/auth.js
+++ src/javascripts/components/auth/auth.js
@@ -1,8 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import util from '../../helpers/util';
+import googleImage from './sign-in-with-google.png';
+
+const signMeIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
 
 const authDomStringBuilder = () => {
-  const domString = 'Auth';
+  let domString = '<button id = "google-auth" class="btn btn-danger">';
+  domString += `<img src=${googleImage} />`;
+  domString += '</button>';
   util.printToDom('auth', domString);
+  document.getElementById('google-auth').addEventListener('click', signMeIn);
 };
 
 export default { authDomStringBuilder };
