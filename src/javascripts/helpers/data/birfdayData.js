b73aa0fdbc34d82f5580e637a01177155c2622a0
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-05-25 12:26:36 -0600

    add friend work

--- src/javascripts/helpers/data/birfdayData.js
+++ src/javascripts/helpers/data/birfdayData.js
@@ -4,7 +4,6 @@ import apiKeys from './apiKeys.json';
 const firebaseUrl = apiKeys.firebaseKeys.databaseURL;
 
 const getBirfdayByUid = uid => new Promise((resolve, reject) => {
-  console.error(uid);
   axios.get(`${firebaseUrl}/birthdays.json?orderBy="uid"&equalTo="${uid}"`)
     .then((results) => {
       const birthdayResults = results.data;
